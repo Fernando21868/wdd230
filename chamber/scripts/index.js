a24f874f491cd4a0bea1bd6b989653989c86d75c
FERNANDO <elderarias2015@gmail.com>
2022-10-29 21:25:58 -0300

    Discover page - completed

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -2,6 +2,18 @@ const hamburger = document.querySelector('.hamburgerBtn');
 const datefield = document.querySelector('.date');
 const actualYear = document.getElementById('actual-year');
 const showBanner = document.querySelector('.banner');
+const navContainer = document.querySelector('nav');
+const navItems = navContainer.getElementsByTagName('a');
+
+// Loop through the nav items and add the active class to the current/clicked nav item
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', function () {
+    let current = document.getElementsByClassName('active');
+    console.log(current);
+    current[0].className = current[0].className.replace('active', '');
+    this.parentElement.className += 'active';
+  });
+}
 
 // Header actual day
 const now = new Date();
@@ -28,3 +40,75 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener('click', toggleMenu);
+
+// Get all imgs with data-src attribute
+const divToLoad = document.querySelectorAll('article[data-class]');
+
+// Optional parameters being set for the IntersectionObserver
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px 50px 0px',
+};
+
+// Lazy Load Images
+const loadImages = (article) => {
+  const img = article.querySelector('img[data-src]');
+  const src = img.getAttribute('data-src');
+  const className = article.getAttribute('data-class');
+  article.setAttribute('class', className);
+  img.setAttribute('src', src);
+  article.removeAttribute('data-class');
+  img.removeAttribute('data-src');
+};
+
+// First check to see if Intersection Observer is supported
+if ('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (!item.isIntersecting) {
+        return;
+      } else {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  divToLoad.forEach((divEl) => {
+    imgObserver.observe(divEl);
+  });
+} else {
+  // Just load all images if not supported
+  divToLoad.forEach((divEl) => {
+    loadImages(divEl);
+  });
+}
+
+// Number of visits
+// initialize display elements
+
+// initialize display elements
+const todayDisplay = document.getElementById('days');
+
+// get the stored value in localStorage
+let numVisits = JSON.parse(localStorage.getItem('visits-ls'));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits) {
+  numVisits.push(Date.now());
+  const timeBetweenLastVisit =
+    Math.floor(((numVisits[numVisits.length - 1] - numVisits[numVisits.length - 2]) /
+      1000) /
+    60 / 60 / 24);
+    todayDisplay.textContent = timeBetweenLastVisit;
+  if (numVisits.length > 2){
+    numVisits.shift();
+  }
+  localStorage.setItem('visits-ls', JSON.stringify(numVisits));
+} else {
+  const visits = [];
+  const first_visit = Date.now();
+  visits.push(first_visit);
+  localStorage.setItem('visits-ls', JSON.stringify(visits));
+  todayDisplay.textContent = 'This is your first visit';
+}
