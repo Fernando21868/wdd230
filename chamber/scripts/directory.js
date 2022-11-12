895e05ce202fd6ae4124dc47c680e9a97c4f2e88
FERNANDO <elderarias2015@gmail.com>
2022-11-11 23:48:07 -0300

    Directory page - javascript file modified (links)

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,16 +1,15 @@
 const requestURL =
   'https://fernando21868.github.io/wdd230/chamber/json/data.json';
 const cards = document.querySelector('.cards');
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector(".cards");
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('.cards');
 
 async function getData() {
   const request = new Request(requestURL);
   const response = await fetch(request);
   const jsonObject = await response.json();
   const companies = jsonObject['companies'];
-  console.log(companies);
   companies.forEach((company) => {
     displayCompanies(company);
   });
@@ -26,8 +25,8 @@ function displayCompanies(company) {
   let membership = document.createElement('p');
   let opening = document.createElement('p');
   let website = document.createElement('a');
-  opening.classList.add('opening-year')
-  membership.classList.add('membership-level')
+  opening.classList.add('opening-year');
+  membership.classList.add('membership-level');
 
   // Change the textContent property of the h2 element to contain the prophet's full name
   h2.textContent = company.name;
@@ -36,11 +35,10 @@ function displayCompanies(company) {
   address.innerHTML = `<strong>Address</strong>: ${company.address}`;
   phone.innerHTML = `<strong>Phone number</strong>: ${company.phonenumber}`;
   membership.innerHTML = `<strong>Membership level</strong>: ${company.membership}`;
-  console.log(company.openingyear);
   opening.innerHTML = `<strong>Opening year</strong>: ${company.openingyear}`;
-  website.textContent='Visit website'
-  website.setAttribute('href',company.website)
-  website.setAttribute('target','_blank')
+  website.textContent = 'Visit website';
+  website.setAttribute('href', company.website);
+  website.setAttribute('target', '_blank');
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   portrait.setAttribute('src', company.imageurl);
@@ -63,24 +61,24 @@ function displayCompanies(company) {
   cards.appendChild(card);
 }
 
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-  const links = document.querySelectorAll(".cards a");
-  links.forEach((link)=>{
-    link.textContent='Visit website'
-  })
+gridbutton.addEventListener('click', () => {
+  display.classList.add('grid');
+  display.classList.remove('list');
+  const links = document.querySelectorAll('.cards a');
+  links.forEach((link) => {
+    link.textContent = 'Visit website';
+  });
 });
 
-listbutton.addEventListener("click", ()=>{
-  display.classList.add("list");
-	display.classList.remove("grid");
-  const links = document.querySelectorAll(".cards a");
-  links.forEach((link)=>{
-    const linkValue=link.getAttribute('href');
-    link.textContent=linkValue
-  })
+listbutton.addEventListener('click', () => {
+  display.classList.add('list');
+  display.classList.remove('grid');
+  const links = document.querySelectorAll('.cards a');
+  links.forEach((link) => {
+    const linkValue = link.getAttribute('href');
+    link.textContent = linkValue;
+    link.setAttribute('target', '_blank');
+  });
 });
-
 
 const prove = getData();
